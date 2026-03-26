67b0d36dd4de96789dbbd0be542d3fcf8e7fdab7
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-03-26 13:52:43 +0530

    add theme toggle dropdown

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+export function Navbar() {
+  return (
+    <nav className="flex w-full items-center justify-between py-5">
+      <div className="flex items-center gap-8">
+        <h1 className="text-3xl font-bold">
+          Next<span className="text-blue-500">Pro</span>
+        </h1>
+      </div>
+    </nav>
+  )
+}
